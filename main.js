abf9a997dd0d90618c7e29fc1eec916d176b2e01
Jacek <trojanowski.jacek@gmail.com>
2020-08-20 13:45:59 +0200

    current section Index

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const rootElement = document.querySelector('[data-root]');
 const sections = [...document.querySelectorAll('[data-section]')]
+let currentSectionIndex = 0
 
 document.addEventListener('mousewheel', (e) => {
-  console.log(sections);
+  const direction = e.wheelDelta > 0 ? 1 : -1;
+
+  if (direction === 1) {
+    const lastSection = currentSectionIndex === sections.length - 1;
+    if (lastSection) return
+  } else if (direction === -1) {
+    const firstSection = currentSectionIndex === 0;
+    if (firstSection) return
+  }
+  currentSectionIndex = currentSectionIndex + direction;
+  console.log(currentSectionIndex);
 
 })
