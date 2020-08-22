382f14a533db8283974768c5fb4b378807868acc
Jacek <trojanowski.jacek@gmail.com>
2020-08-22 19:59:39 +0200

    add swiper for mobile

---
+++ swiper.js
@@ -0,0 +1,36 @@
+class Swiper {
+  constructor() {
+    this.initialY = null;
+
+    document.addEventListener('touchstart', (e) => this.startTouch(e));
+    document.addEventListener('touchmove', (e) => this.moveTouch(e));
+
+    this.events = {
+      swipeUp: new Event('swipeUp'),
+      swipeDown: new Event('swipeDown'),
+    }
+  }
+  startTouch(e) {
+    e.preventDefault()
+    this.initialY = e.touches[0].clientY;
+  }
+
+  moveTouch(e) {
+    console.log(this.initialY)
+    if (!this.initialY) {
+      return
+    }
+    const currentY = e.touches[0].clientY;
+    const deltaY = this.initialY - currentY;
+    if (deltaY > 0) {
+      document.dispatchEvent(this.events.swipeDown);
+    } else {
+      document.dispatchEvent(this.events.swipeUp);
+    }
+    this.initialY = null
+  }
+
+}
+
+new Swiper();
+
